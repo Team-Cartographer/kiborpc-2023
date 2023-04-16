cb7a23101443d28ce072f890ca1e0b57fdb3d0ab
abhi arya <abhia9167@gmail.com>
2023-04-16 14:54:37 -0700

    Got apriltag detection working with example image, but not actual image

--- TagDetector.py
+++ TagDetector.py
@@ -1,1 +1,15 @@
-print("Hello WOrld")
+import cv2
+import numpy as np
+import pupil_apriltags as p
+
+#TAG = 'exampleTag.png'
+TAG = 'tagformats_web.png'
+tag_family = 'tag16h5'
+detector = p.Detector(families=tag_family, nthreads=10)
+tags = detector.detect(cv2.cvtColor(cv2.imread(TAG), cv2.COLOR_BGR2GRAY))
+img = cv2.polylines(cv2.imread(TAG), 
+                    [np.array(tag.corners, dtype=np.int32).reshape((-1, 1, 2)) for tag in tags], 
+                    True, (255, 0, 0), 2
+                    )
+cv2.imshow('AprilTags', img)
+cv2.waitKey(0)
